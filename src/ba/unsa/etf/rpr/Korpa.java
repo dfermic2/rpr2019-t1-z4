bc313ed56e257837661d2dc16912abee3f7c6195
dfermic2 <dfermic2@etf.unsa.ba>
2020-10-28 18:56:11 +0100

    Nesto mozda radi

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,42 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    Artikl[] artikli;
+
+    public Korpa() {
+        this.artikli = new Artikl[50];
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+        for(int i = 0; i < 50; i++) {
+            if(artikli[i] == null) {
+                artikli[i] = a;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i = 0; i < artikli.length; i++) {
+            if(artikli[i] != null && artikli[i].kod.equals(kod)) {
+                Artikl temp = artikli[i];
+                artikli[i] = null;
+                return temp;
+            }
+        }
+        return null;
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+        int ukupnaCijena = 0;
+        for (int i = 0; i < 50; i ++) {
+            if (artikli[i] != null) ukupnaCijena = ukupnaCijena + artikli[i].cijena;
+        }
+        return ukupnaCijena;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,34 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    Artikl [] artikli;
+
+    public Supermarket() {
+        this.artikli = new Artikl[1000];
+    }
+
+    public boolean dodajArtikl(Artikl artikl) {
+        for(int i = 0; i < artikli.length; i++) {
+            if(artikli[i] == null) {
+                artikli[i] = artikl;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Artikl[] getArtikli() {
+        return artikli;
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+        for (int i = 0; i < artikli.length; i++) {
+            if(artikli[i] != null && artikli[i].kod.equals(kod)) {
+                Artikl temp = artikli[i];
+                artikli[i] = null;
+                return temp;
+            }
+        }
+        return null;
+    }
 }
